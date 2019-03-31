f9b90c1877ab7ffd286da3cc35d3987a138d8d9d
Jaiden Meiden <jaidenmeiden@MacBook-Air-de-Diana-2.local>
2019-03-30 22:50:37 -0500

    ::FundamentosJS:: Funciones

---
+++ Funciones/clase4.js
@@ -0,0 +1,17 @@
+var nombre = 'Jaiden', edad = 38;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Diana', 28);
+imprimirEdad('David', 30);
+imprimirEdad('Milena', 35);
+
+//JavaScript es un lenguaje interpretado
+//Lenguaje debilmente tipado y siempre va a intentar ejecutar el código
+//Ejemplos
+imprimirEdad(35, 'Fulano');
+imprimirEdad('Fulano');
+imprimirEdad();
