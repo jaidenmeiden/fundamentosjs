20ccc0c4f25340ca134051b96ff2f50963ba1a84
Jaiden Riaño <jaidenmeiden@MacBook-Air-de-Diana-2.local>
2019-03-30 20:30:11 -0500

    ::FundamentosJS:: Variables

---
+++ Variables/clase1.js
@@ -0,0 +1,13 @@
+//console.log("Hola mundo JS!");
+
+var nombre = 'Jaiden', apellido = 'Meiden';
+
+//Nada en JavaScript impide que se modifique una variable de un tipo especifico, 
+//por ejemplo 'integer' y se a juste a un tipo 'String' como sigue
+var edad = 28;
+edad = '28 años';
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
