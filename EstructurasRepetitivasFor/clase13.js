29ac297c4fa78caa88daa1e3cb611692396866a8
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-06-18 08:08:26 -0500

    Estructuras repetitivas: for

--- EstructurasRepetitivasFor/clase13.js
+++ EstructurasRepetitivasFor/clase13.js
@@ -15,16 +15,16 @@ var juan = {
 const DIAS_DEL_ANO = 365;
 const INCREMENTO_PESO= 0.25;
 
-const AUMENTO_PESO = persona => persona.peso += INCREMENTO_PESO;
-const REDUCE_PESO = persona => persona.peso -= INCREMENTO_PESO;
+const aumentaDePeso = persona => persona.peso += INCREMENTO_PESO;
+const reduceDePeso = persona => persona.peso -= INCREMENTO_PESO;
 
 console.log(`Al inico del año ${jaiden.nombre} pesa ${jaiden.peso}Kg`);
 for (var i = 1; i<= DIAS_DEL_ANO; i++) {
     var random = Math.random();//Esta función arroja un aleatorio entre 0...1
     if(random < 0.25){
-        AUMENTO_PESO(jaiden);
+        aumentaDePeso(jaiden);
     }else if(random < 0.50){
-        REDUCE_PESO(jaiden);
+        reduceDePeso(jaiden);
     }
 }
 console.log(`Al final del año ${jaiden.nombre} pesa ${jaiden.peso.toFixed(2)}Kg`);
