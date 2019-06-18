e1f9ce5a53c535b23e60b7aee402334c12189cf7
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-06-18 07:11:41 -0500

    Funciones que retornan valores

---
+++ FuncionesRetorno/clase11.js
@@ -0,0 +1,34 @@
+var jaiden = {
+    nombre: 'Jaiden',
+    apellido: 'Meiden',
+    edad: 38,
+    ingeniero: true,
+    cocinero: true,
+    cantante: false,
+    dj: false,
+    guitarrista: false,
+    ciclista: true
+};
+
+var juan = {
+    nombre: 'Juan',
+    apellido: 'Cifuentes',
+    edad: 16
+};
+
+const MAYORIA_DE_EDAD = 18;
+
+function esMayorDeEdad (persona) {
+    return persona.edad >= MAYORIA_DE_EDAD;
+}
+
+function imprimirSiEsMayorDeEdad (persona) {
+    if(esMayorDeEdad(persona)) {
+        return `${persona.nombre} es mayor de edad`;
+    } else {
+        return `${persona.nombre} es menor de edad`;
+    }
+}
+
+console.log(imprimirSiEsMayorDeEdad(jaiden));
+console.log(imprimirSiEsMayorDeEdad(juan));
