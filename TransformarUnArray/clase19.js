755aff651d404191c473a2e5d76924bcd0efca9c
Jaiden Meiden <jaidenmeiden@gmail.com>
2019-06-26 04:34:31 -0500

    Se agrega ejemplo de afectación de propiedades compartidas por varios objetos

--- TransformarUnArray/clase19.js
+++ TransformarUnArray/clase19.js
@@ -1,30 +1,35 @@
 var jaiden = {
     nombre: 'Jaiden',
     apellido: 'Meiden',
+    peso: 97,//En Kg
     altura: 1.83
 };
 
 var sergio = {
     nombre: 'Sergio',
     apellido: 'Hernandez',
+    peso: 70,//En Kg
     altura: 1.70
 };
 
 var felipe = {
     nombre: 'Felipe',
     apellido: 'Hernandez',
+    peso: 68,//En Kg
     altura: 1.70
 };
 
 var carlos = {
     nombre: 'Carlos',
     apellido: 'Niño',
+    peso: 80,//En Kg
     altura: 1.85
 };
 
 var daniel = {
     nombre: 'Daniel',
     apellido: 'Jimenez',
+    peso: 60,//En Kg
     altura: 1.65
 };
 
@@ -33,18 +38,32 @@ var personas = [jaiden, sergio, felipe, carlos, daniel];
 console.warn('personas: ');
 console.log(personas);
 const esAlta = ({ altura }) => altura > 1.8;
+
+//Si se utiliza la función a continuación "pasarAlturaACms" 
+//se modifica directamente las propiedades del objeto 
+//persona y por dicha razón se ven afectados los demas 
+//objetos que usan dichas propiedades.
+//Para que esto no sea perceptible por otrs objetos se debe
+//crear un nuevo objeto.
+const pasarPesoAGramos = persona => {
+    persona.peso *= 1000;//Se transforma a gramos
+    return persona
+}
 const pasarAlturaACms1 = persona => {
-    //persona.altura *= 100;
-    //return persona
     return {//Retorna un nuevo objeto 
         ...persona,
         altura: persona.altura * 100
     };
 }
-const pasarAlturaACms2 = persona => ({
+const pasarAlturaACms2 = persona => ({//Retorna un nuevo objeto 
     ...persona,
     altura: persona.altura * 100
 })
+
+var personasGramos = personas.filter(pasarPesoAGramos);
+
+console.warn('personasGramos: ');
+console.log(personasGramos);
 
 var personasAltas = personas.filter(esAlta);
 
