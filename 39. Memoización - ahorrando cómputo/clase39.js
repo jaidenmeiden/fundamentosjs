f506d94fada3540f8b147448af8a8a8f3869904f
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-10 10:22:18 -0500

    Memorización: ahorrando cómputo

---
+++ 39. Memoización - ahorrando cómputo/clase39.js
@@ -0,0 +1,42 @@
+/*
+    Factorial de 6
+    !6 = 6 * 5 * 4 * 3 * 2 * 1 = 720
+
+    Aquí calculos equivalentes:
+    !12 = 12 * 11 * 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+    !12 = 12 * 11 * 10 * 9 * 8 * 7 * !6
+ */
+
+function factorial(n) {//Función recursiva normal
+    if(!this.cache) {
+        this.cache = {};
+    }
+
+    if(n === 1) {
+        return n;
+    }
+
+    return n * factorial(n-1);
+}
+
+//Almacena en la cacha calculos previos para ser utilizados cuando se requiera
+function factorialCache(n) {
+    if(!this.cache) {//Validamos si cache existe
+        this.cache = {};//La inicializamos si no
+    }
+
+    if(this.cache[n]) {//Validamos si el calculo para el factorial, ya fue hecho previamente
+        return this.cache[n];//Si sí, lo utilzamos
+    }
+
+    if(n === 1) {
+        return n;
+    }
+
+    this.cache[n] = n * factorialCache(n-1);
+    return this.cache[n];
+}
+
+
+//console.log(factorial(6));
+console.log(factorialCache(6));
